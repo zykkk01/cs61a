1a2d67cffb4b1b7510b13af9213a4559ababef17
zykkk01 <3230103354@zju.edu.cn>
2024-09-07 20:40:10 +0800

    add files

---
+++ lab/lab01/test.py
@@ -0,0 +1,16 @@
+def find_digit(k):
+    """Returns a function that returns the kth digit of x.
+
+    >>> find_digit(2)(3456)
+    5
+    >>> find_digit(2)(5678)
+    7
+    >>> find_digit(1)(10)
+    0
+    >>> find_digit(4)(789)
+    0
+    """
+    assert k > 0
+    "*** YOUR CODE HERE ***"
+    return lambda s:int(str(s)[-k]) if k<=len(str(s)) else 0
+
---
+++ lab/lab02/test.py
@@ -0,0 +1,20 @@
+def swipe(n):
+    """Print the digits of n, one per line, first backward then forward.
+
+    >>> swipe(2837)
+    7
+    3
+    8
+    2
+    8
+    3
+    7
+    """
+    if n < 10:
+        print(n)
+    else:
+        "*** YOUR CODE HERE ***"
+        print(n%10)
+        swipe(n//10)
+        print(n%10)
+
